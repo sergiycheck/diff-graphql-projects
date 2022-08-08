afb992825c0f394351cb9637d20ce5656397cf23
sergiycheck <sergiycheck@gmail.com>
2022-08-08 17:50:40 +0300

    configured ssl

--- leaning_from_docs/apollo-security-auth/src/index.ts
+++ leaning_from_docs/apollo-security-auth/src/index.ts
@@ -11,6 +11,9 @@ import {
 } from 'apollo-server-core';
 import express from 'express';
 import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 import { loadFiles } from '@graphql-tools/load-files';
 import DataLoader from 'dataloader';
 import { getUsersByIds, getPostsByIds } from './blog/common-data';
@@ -26,11 +29,48 @@ import { User } from './blog/types';
 
 async function startApolloServer(typeDefs, resolvers) {
   const app = express();
-  const httpServer = http.createServer(app);
+
+  const configurations = {
+    // Note: You may need sudo to run on port 443
+    production: { ssl: true, port: process.env.PROD_PORT, hostname: 'localhost' },
+    development: { ssl: false, port: process.env.DEV_PORT, hostname: 'localhost' },
+  };
+
+  const environment = process.env.NODE_ENV || 'production';
+  const config = configurations[
+    environment
+  ] as typeof configurations[keyof typeof configurations];
+
+  let httpServer: https.Server | http.Server;
+
+  if (config.ssl) {
+    // Assumes certificates are in a .ssl folder off of the package root.
+    // Make sure these files are secured.
+    const keyFullPath = path.resolve(
+      process.cwd(),
+      `./.ssl/${environment}/${process.env.SSL_PRIVATE_KEY}`
+    );
+    const certFullPath = path.resolve(
+      process.cwd(),
+      `./.ssl/${environment}/${process.env.SSL_CERTIFICATE}`
+    );
+    httpServer = https.createServer(
+      {
+        key: fs.readFileSync(keyFullPath),
+        cert: fs.readFileSync(certFullPath),
+      },
+
+      app
+    );
+  } else {
+    httpServer = http.createServer(app);
+  }
 
   const server = new ApolloServer({
-    typeDefs: await loadFiles('src/**/*.{gql, graphql}'),
-    resolvers: await loadFiles('src/**/*.resolver.ts'),
+    typeDefs: await loadFiles('schema/**/*.{gql, graphql}'),
+    resolvers: await loadFiles(
+      `src/**/*.resolver.${environment === 'development' ? 'ts' : 'js'}`
+    ),
     csrfPrevention: true,
     cache: 'bounded',
 
@@ -86,10 +126,17 @@ async function startApolloServer(typeDefs, resolvers) {
     origin: ['https://www.your-app.example', 'https://studio.apollographql.com'],
     credentials: true,
   };
+
   server.applyMiddleware({ app, cors: corsOptions });
-  const port = process.env.PORT;
-  await new Promise<void>((resolve) => httpServer.listen({ port: port }, resolve));
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: config.port }, resolve));
+
+  console.log(
+    '🚀 Server ready at',
+    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+      server.graphqlPath
+    }`
+  );
 }
 
 startApolloServer({}, {});
